d4506c6740a5fc16c05644ce0504758e798176c6
Kevin Yeh <revinyeh@gmail.com>
2023-12-07 09:59:14 -0800

    add form input label compoments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,16 +17,6 @@ export default function Home() {
 
   return (
     <div className="p-4">
-      <Modal
-        isOpen={true}
-        title={"Test"}
-        description={"Test"}
-        onClose={() => {
-          throw new Error("Function not implemented.");
-        }}
-      >
-        Children
-      </Modal>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
