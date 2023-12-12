317006481990650878c3ddfb1fa642be3244bed7
Kevin Yeh <revinyeh@gmail.com>
2023-12-11 20:54:44 -0800

    adding command

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,9 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
+const prismadb =
+  globalThis.prisma /* globalThis.prisma to avoid create more then one prisma db */ ||
+  new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
